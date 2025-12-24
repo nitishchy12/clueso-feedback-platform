11fd3a6d1c203eaf7f6824a016fb879b025a5615
Nitish <nitishchy32@gmail.com>
2025-12-24 18:16:37 +0530

    Clean project: Remove unnecessary files, finalize Docker setup

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Clueso.io Clone...
-echo.
-echo Backend will start on: http://localhost:5000
-echo Frontend will start on: http://localhost:3000
-echo.
-echo Press Ctrl+C to stop both servers
-echo.
-npm run dev
